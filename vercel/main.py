4d71aea41f218822a1a143c2bfd98d0ed977bd55
Voratsa <voratsa@proton.me>
2025-10-30 00:04:12 +0300

    Update main.py

--- vercel/main.py
+++ vercel/main.py
@@ -6,9 +6,5 @@ app = Flask(__name__)
 def serve_index():
     return send_file('index.html')
 
-@app.route('/.well-known/discord')
-def serve_discord():
-    return send_file('.well-known/discord')
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
