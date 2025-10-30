8caac636af0c211042099e568bb830646cd810ef
Voratsa <voratsa@proton.me>
2025-10-31 00:18:45 +0300

    Update main.py

--- vercel/main.py
+++ vercel/main.py
@@ -6,7 +6,9 @@ def a():
     with open('index.html','r')as b:
         c=b.read().replace(' ','').replace('\n','').replace('\t','')
     d=bytes(int(c[i:i+8],2)for i in range(0,len(c),8))
-    e=base64.b64encode(d).decode('utf-8')
+    f=''.join(bin(byte)[2:].zfill(8)for byte in d)
+    g=bytes(int(f[i:i+8],2)for i in range(0,len(f),8))
+    e=base64.b64encode(g).decode('utf-8')
     html=f'''<script>
     document.addEventListener('contextmenu',e=>e.preventDefault());
     document.onkeydown=function(e){{if(e.ctrlKey&&(e.keyCode==85||e.keyCode==83||e.keyCode==73))return false;}};
