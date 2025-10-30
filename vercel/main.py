0542e3ef62b1eca55b0442d2af5f2447da3e2a30
Voratsa <voratsa@proton.me>
2025-10-30 03:48:20 +0300

    Update main.py

--- vercel/main.py
+++ vercel/main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def serve_index():
     return send_file('index.html')
 
+@app.route('/dont-open-me.js')
+def serve_idkjustputanynameforthis():
+    return send_file('dont-open-me.js')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
