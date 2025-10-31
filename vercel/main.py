3c9a29c0e2640d3477bc8ac43e5518ee8c36e7f9
Voratsa <voratsa@proton.me>
2025-10-31 05:43:22 +0300

    Update main.py

--- vercel/main.py
+++ vercel/main.py
@@ -1,20 +1,14 @@
-from flask import Flask, Response, send_from_directory
-import base64
-app = Flask(__name__)
-@app.route('/')
-def a():
-    with open('index.html','r')as b:
-        c=b.read().replace(' ','').replace('\n','').replace('\t','')
-    d=bytes(int(c[i:i+8],2)for i in range(0,len(c),8))
-    f=d.decode('utf-8').replace(' ','').replace('\n','').replace('\t','')
-    g=bytes(int(f[i:i+8],2)for i in range(0,len(f),8))
-    e=base64.b64encode(g).decode('utf-8')
-    html=f'''<script>
-    document.addEventListener('contextmenu',e=>e.preventDefault());
-    document.onkeydown=function(e){{if(e.ctrlKey&&(e.keyCode==85||e.keyCode==83||e.keyCode==73))return false;}};
-    document.write(decodeURIComponent(escape(atob("{e}"))));
-    </script>'''
-    return Response(html,mimetype='text/html; charset=utf-8')
+from flask import Flask, send_file
 
-if __name__=='__main__':
-    app.run(host='0.0.0.0',port=5000)
+app = Flask(__name__)
+
+@app.route('/')
+def serve_index():
+    return send_file('index.html')
+
+@app.route('/script.js')
+def send_script():
+    return send_file('script.js')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
