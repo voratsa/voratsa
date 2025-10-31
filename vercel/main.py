4a140ed0fb6be7aaf1d79d7b032015cb5a495f6f
Voratsa <voratsa@proton.me>
2025-10-31 08:40:31 +0300

    Update main.py

--- vercel/main.py
+++ vercel/main.py
@@ -6,13 +6,5 @@ app = Flask(__name__)
 def serve_index():
     return send_file('index.html')
 
-@app.route('/script.js')
-def send_js():
-    return send_file('script.js')
-
-@app.route('/style.css')
-def send_css():
-    return send_file('style.css')
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
