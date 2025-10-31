9be440b032e64896df91fbf2dcc1725074e11547
Voratsa <voratsa@proton.me>
2025-10-31 05:45:16 +0300

    Update main.py

--- vercel/main.py
+++ vercel/main.py
@@ -7,8 +7,13 @@ def serve_index():
     return send_file('index.html')
 
 @app.route('/script.js')
-def send_script():
+def send_js():
     return send_file('script.js')
+
+
+@app.route('/style.css')
+def send_css():
+    return send_file('style.css')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
