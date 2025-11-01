215a50125728e9b9e7647daff270579b5ce0ecb9
Voratsa <voratsa@proton.me>
2025-11-01 08:15:34 +0300

    Update main.py

--- vercel/main.py
+++ vercel/main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def serve_index():
     return send_file('index.html')
 
+@app.route('/googlea00fdca30afc15b7.html')
+def serveea():
+    return send_file('googlea00fdca30afc15b7.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
