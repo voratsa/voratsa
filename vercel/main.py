366dabfc7818b8c62bb00cb83b699f8d065093d3
Voratsa <voratsa@proton.me>
2025-11-01 08:18:18 +0300

    Update main.py

--- vercel/main.py
+++ vercel/main.py
@@ -10,5 +10,9 @@ def serve_index():
 def serveea():
     return send_file('g/googlea00fdca30afc15b7.html')
 
+@app.route('/sitemap.xml')
+def servea():
+    return send_file('g/sitemap.xml')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
